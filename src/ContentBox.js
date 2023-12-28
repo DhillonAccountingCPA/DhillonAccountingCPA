89e3def2270dbb1ca6f0fc7f5056fef3d6b50440
Harnoor Dhillon <hdhillon.cpa@gmail.com>
2023-12-27 20:32:25 -0800

    fixed all formatting hopefully

--- src/ContentBox.js
+++ src/ContentBox.js
@@ -6,9 +6,9 @@ const ContentBox = () => {
     <div className="content-box">
       <h1 className="content-heading">Working with you for a better financial future</h1>
       <p className="content-text">Dhillon Accounting CPA is a full-service accounting firm dedicated to maximizing your financial benefits for both Personal and Business Taxes.</p>
-      <p className="offer">FOR FREE 30 MINUTE CONSULTATION</p>
+      <p className="offer">FOR FREE CONSULTATION</p>
       <a href="https://calendly.com/hdhillon-cpa" target="_blank" rel="noopener noreferrer" className="start-button-link">
-        <button className="start-button">Get Started Now</button>
+        <button className="start-button">Book Appointment</button>
       </a>
     </div>
   );
