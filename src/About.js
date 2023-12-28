b33769cfda5427b325b4e7358967aad92abe1974
Harnoor Dhillon <hdhillon.cpa@gmail.com>
2023-12-27 21:07:34 -0800

    format

--- src/About.js
+++ src/About.js
@@ -14,7 +14,7 @@ const About = () => {
       <div className="about-divider"></div>
       <div className="about-right">
         <TypingText />
-        <p>Situated in Cloverdale area of Surrey, British Columbia, <span className="highlighted">Dhillon Accounting CPA Inc.</span> is a professional accounting firm established with the <span className="highlighted">vision</span> of providing clients with financial tools and results they need, so that they can prosper in what they <span className="highlighted">love</span> to do in their personal or business life. With our <span className="highlighted">dedicated team</span>, we have catered to individual and small to medium sized businesses clients in and around Metro Vancouver and Fraser Valley <span className="highlighted">since 2016</span>. </p>
+        <p>Situated in Cloverdale area of Surrey, British Columbia, <span className="highlighted">Dhillon Accounting CPA Inc.</span> is a professional accounting firm established with the <span className="highlighted">vision</span> of providing clients with financial tools and results they need, so that they can prosper in what they <span className="highlighted">love</span> to do in their personal or professional life. With our <span className="highlighted">dedicated team</span>, we have catered to individual and small to medium sized business clients in and around Metro Vancouver and Fraser Valley <span className="highlighted">since 2016</span>. </p>
       </div>
     </Element>
   );
